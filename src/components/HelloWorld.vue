378ea95e720e3fc38eef2bc86d9b06050f906ecf
王柳 <wangliu@kingchannels.com>
2018-01-24 17:03:51 +0800

    提交

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+     <mt-button @click.native="handleClick">按钮</mt-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  methods:{
+     handleClick() {
+      this.$toast('Hello world!')
+    }
+  }
+}
+</script>
+
+<style >
+
+</style>
